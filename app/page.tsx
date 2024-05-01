31882c264529901433a955ae57b045815645aee6
Jonatas Abreu <96961198+JonatasAbreu@users.noreply.github.com>
2024-05-01 13:16:01 -0300

    feat: add promo banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search";
@@ -9,9 +10,20 @@ export default function Home() {
 
       <div className=" px-5 pt-6">
         <Search />
-        <div className="px-5 pt-6">
-          <CategoryList />
-        </div>
+      </div>
+      <div className="px-5 pt-6">
+        <CategoryList />
+      </div>
+      <div className="px-5 pt-6">
+        <Image
+          src="/promo-banner-01.png"
+          alt="Até 30% de desconto em pizza"
+          width={0}
+          height={0}
+          className="h-auto w-full object-contain"
+          sizes="100vw"
+          quality={100}
+        />
       </div>
     </>
   );
