062e21b57fef315dd6e0237b7f83ddd893e046fd
Jonatas Abreu <96961198+JonatasAbreu@users.noreply.github.com>
2024-05-02 17:36:57 -0300

    feat: promo banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search";
@@ -6,6 +5,7 @@ import ProductLits from "./_components/product-list";
 import { Button } from "./_components/ui/button";
 import { ChevronRightIcon } from "lucide-react";
 import { db } from "./_lib/prisma";
+import PromoBanner from "./_components/promo-banner";
 
 const Home = async () => {
   const products = await db.product.findMany({
@@ -35,17 +35,10 @@ const Home = async () => {
       <div className="px-5 pt-6">
         <CategoryList />
       </div>
-      <div className="px-5 pt-6">
-        <Image
-          src="/promo-banner-01.png"
-          alt="Até 30% de desconto em pizza"
-          width={0}
-          height={0}
-          className="h-auto w-full object-contain"
-          sizes="100vw"
-          quality={100}
-        />
-      </div>
+      <PromoBanner
+        src="/promo-banner-01.png"
+        alt="Até 30% de desconto em pizza"
+      />
       <div className="space-y-4 pt-6">
         <div className="flex items-center justify-between px-5">
           <h2 className="font-semibold">Pedidos Recomendados</h2>
@@ -59,6 +52,12 @@ const Home = async () => {
         </div>
 
         <ProductLits products={products} />
+      </div>
+      <div className="px-5 pt-6">
+        <PromoBanner
+          src="/promo-banner-02.png"
+          alt="lanches a partir de 17,90"
+        />
       </div>
     </>
   );
